71e9bc22ca74047ca2ddb9504e332e122f0dcbd5
SaxenaSim <saxenasimran84@gmail.com>
2024-03-09 22:36:42 +0530

    Modify test cases

--- tests/test_data_file.py
+++ tests/test_data_file.py
@@ -3,16 +3,19 @@ from src.detection import detection
 
 objDetection = detection()
 
+# testing data file with default value
 def test_rdf_empty_name():
     example_string=""
     result = objDetection.read_data_file(example_string)
     assert result == "data/data.txt"
-    
+ 
+ # testing  data file with wrong filename
 def test_rdf_wrong_name():
     example_string="myTest.txt"
     result = objDetection.read_data_file(example_string)
     assert result != "data/data.txt"
-    
+ 
+ # testing data file with user filename   
 def test_rdf_cust_name():
     example_string="testData.txt"
     result = objDetection.read_data_file(example_string)
